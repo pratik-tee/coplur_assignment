088aef9239fba6f8403ba3a75a7408b96599ae84
pratik-tee <pratikteenwal8@gmail.com>
2025-06-22 16:10:57 +0530

    q1

---
+++ day3/q1.py
@@ -0,0 +1,9 @@
+def maths(a=0, b=0):
+    print("Addition:", a + b)
+    print("Subtraction:", a - b)
+    print("Multiplication:", a * b)
+    print("division",a/b)
+a = int(input("enter the a value"))
+b = int(input("enter the b value "))
+
+maths(a, b)
